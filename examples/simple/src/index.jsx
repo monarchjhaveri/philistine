3978027a1fcc603f273bcf28dcb9c72c10a04497
Monarch Wadia <monarchwadia@gmail.com>
2018-04-12 12:27:26 -0400

    Cleaned up example

--- examples/simple/src/index.jsx
+++ examples/simple/src/index.jsx
@@ -1,30 +1,6 @@
-
-
-// class App extends React.Component {
-//   render() {
-//     return (<div>
-//       <h1> App </h1>
-//       <j.Context.Consumer>
-//         { 
-//           ({message, changeMessage}) => (<div>
-//             {message}
-//             rootComponent
-//             <button onClick={ () => j.changeMessage('ok3') } />
-//           </div>)
-//         }
-//       </j.Context.Consumer>
-//     </div>)
-//   }
-// }
-
-
 import Philistine, {partial} from '../../../philistine/src/philistine.jsx';
 import React from 'react';
 import ReactDom from 'react-dom';
-
-const initialState = {message: 'Not working yet!'};
-
-const phil = new Philistine(initialState);
 
 class App extends React.Component {
   render() {
@@ -38,4 +14,6 @@ class App extends React.Component {
   }
 }
 
+const initialState = {message: 'Not working yet!'};
+const phil = new Philistine(initialState);
 phil.render(<App />, document.getElementById('out'));
