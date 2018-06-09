0e50cc2e72f1a34aad0e301b8a1f15d5decc9f61
Monarch <monarchwadia@gmail.com>
2018-06-08 22:05:53 -0400

    Updated simple example

--- examples/simple/src/index.jsx
+++ examples/simple/src/index.jsx
@@ -1,15 +1,25 @@
-import Philistine, {partial} from '../../../philistine/src/Philistine.jsx';
 import React from 'react';
-import ReactDom from 'react-dom';
+import Philistine from '../../../philistine/src/Philistine.jsx';
 
+// Our app
 class App extends React.Component {
+  getMessage(phil) {
+    // retrieve the current message from global state
+    return <div>{ phil.state.message }</div>;
+  }
+
+  updateMessage() {
+    // when the button is clicked, update the message
+    phil.dispatch('UPDATE_MESSAGE', 'Hello World!');
+  }
+
   render() {
     return (
       <div>
-        <phil.partial>
-          { ({message}) => message }
-        </phil.partial>
-        <button onClick={() => phil.update({message: { $set: 'Hello World!' }})}>
+        <phil.consumer>
+          {this.getMessage}
+        </phil.consumer>
+        <button onClick={this.updateMessage}>
           Click me
         </button>
       </div>
@@ -17,6 +27,16 @@ class App extends React.Component {
   }
 }
 
-const initialState = {message: 'Not working yet!'};
-const phil = new Philistine(initialState);
-phil.render(<App />, document.getElementById('out'));
+// set the initial state and the options, then create the Philistine instance
+const initialState = {message: 'Click the button below to get a special message'};
+const options = { template: App }
+const phil = new Philistine(initialState, options);
+
+// Add the handler for the button
+phil.handle('UPDATE_MESSAGE', (draftState, newMessage) => {
+  draftState.message = newMessage;
+  return draftState;
+});
+
+// render the app
+phil.render(document.getElementById('root'));
